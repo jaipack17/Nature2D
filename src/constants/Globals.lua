ff922cfebc416e9a50d013ddc7763ef2b59e527a
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-10-11 16:11:09 +0530

    global constants

---
+++ src/constants/Globals.lua
@@ -0,0 +1,28 @@
+return {
+	engineInit = {
+		gravity = Vector2.new(0, .3),
+		friction = 0.99,
+		bounce = 0.8,
+		timeSteps = 1,
+		canvas = {
+			topLeft = Vector2.new(0, 0),
+			size = workspace.CurrentCamera.ViewportSize,
+		},
+	},
+	universalMass = 1,
+	properties = {
+		"gravity",
+		"friction",
+		"collisionmultiplier",
+	},
+	constraint = {
+		color = Color3.new(1),
+		thickness = 4,
+	},
+	point = {
+		radius = 2.5,
+		color = Color3.new(1),
+		uicRadius = UDim.new(1, 0),
+	},
+	offset = Vector2.new(0, 36),
+}
