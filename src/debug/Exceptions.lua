d9f3d836d6bef8376b0d349177761a4cb96fb8be
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-11-08 19:16:51 +0530

    Add INVALID_CONSTRAINT_LENGTH Exception

--- src/debug/Exceptions.lua
+++ src/debug/Exceptions.lua
@@ -6,7 +6,8 @@ local TYPES = {
 	NO_CANVAS_FOUND = "[Nature2D]: No canvas found, initialize the engine's canvas using Engine:CreateCanvas().",
 	NO_RIGIDBODIES_FOUND = "[Nature2D]: No rigid bodies found on start.",
 	PROPERTY_NOT_FOUND = "[Nature2D]: Invalid Argument #1. Property not found.",
-	INVALID_CONSTRAINT_TYPE = "[Nature2D]: Received Invalid Constraint Type."
+	INVALID_CONSTRAINT_TYPE = "[Nature2D]: Received Invalid Constraint Type.",
+	INVALID_CONSTRAINT_LENGTH = "[Nature2D]: Received Invalid Constraint Length."
 }
 
 return function (TASK: string, TYPE: string)
