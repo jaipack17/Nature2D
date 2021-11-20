137e510845e7532463a0bf90ef05846d4cbb4878
jaipack17 <jaikaransingh2007@outlook.com>
2021-11-20 16:10:07 +0530

    Update Exceptions and TypeErrors

--- src/debug/Exceptions.lua
+++ src/debug/Exceptions.lua
@@ -1,16 +1,17 @@
---[[
-	Handling exceptions
-]]--
+-- Handling exceptions
 
 local TYPES = {
-	NO_CANVAS_FOUND = "[Nature2D]: No canvas found, initialize the engine's canvas using Engine:CreateCanvas().",
-	NO_RIGIDBODIES_FOUND = "[Nature2D]: No rigid bodies found on start.",
-	PROPERTY_NOT_FOUND = "[Nature2D]: Invalid Argument #1. Property not found.",
-	INVALID_CONSTRAINT_TYPE = "[Nature2D]: Received Invalid Constraint Type.",
-	INVALID_CONSTRAINT_LENGTH = "[Nature2D]: Received Invalid Constraint Length.",
-	INVALID_CONSTRAINT_THICKNESS = "[Nature2D]: Received Invalid Constraint Thickness.",
-	SAME_ID = "[Nature2D]: Cannot ignore collisions for the same RigidBodies.",
-	INVALID_RIGIDBODY = "[Nature2D]: Received Invalid RigidBody."
+	NO_CANVAS_FOUND = "No canvas found, initialize the engine's canvas using Engine:CreateCanvas().",
+	NO_RIGIDBODIES_FOUND = "No rigid bodies found on start.",
+	PROPERTY_NOT_FOUND = "Invalid Argument #1. Property not found.",
+	INVALID_CONSTRAINT_TYPE = "Received Invalid Constraint Type.",
+	INVALID_CONSTRAINT_LENGTH = "Received Invalid Constraint Length.",
+	INVALID_CONSTRAINT_THICKNESS = "Received Invalid Constraint Thickness.",
+	SAME_ID = "Cannot ignore collisions for the same RigidBodies.",
+	INVALID_RIGIDBODY = "Received Invalid RigidBody.",
+	INVALID_OBJECT = "Received an Invalid Object. Valid objects - RigidBody, Point and Constraint",
+	INVALID_PROPERTY = "Received an Invalid Object Property.",
+	MUST_HAVE_PROPERTY = "Missing must-have properties.",
 }
 
 return function (TASK: string, TYPE: string)
@@ -18,7 +19,7 @@ return function (TASK: string, TYPE: string)
 		if TASK == "warn" then 
 			warn(TYPES[TYPE])
 		elseif TASK == "error" then 
-			error(TYPES[TYPE], 2)
+			error("[Nature2D]: "..TYPES[TYPE], 2)
 		end
 	end
 end
--- src/debug/TypeErrors.lua
+++ src/debug/TypeErrors.lua
@@ -1,9 +1,16 @@
---[[
-	Handling type errors
-]]--
+-- Handling type errors
 
 return function (arg: string, param, pos: number, expected: string)
-	if typeof(param) ~= expected then 
-		error("[Nature2D]: Invalid Argument #"..pos..". Expected "..expected.." for '"..arg.."', got "..typeof(param)..".", 2)
+	if typeof(param) ~= expected then
+		error(
+			string.format(
+				"[Nature2D]: Invalid Argument #%s. Expected type %q for %s, got %q",
+				tostring(pos),
+				expected,
+				arg,
+				typeof(param)
+			),	
+			2
+		)
 	end
 end
