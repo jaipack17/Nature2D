0bb4dae14544cb1c9de727bce873754a5a737ed2
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-12-31 14:07:29 +0530

    Update Line.draw()

--- src/Utilities/Line.lua
+++ src/Utilities/Line.lua
@@ -4,26 +4,33 @@
 local Globals = require(script.Parent.Parent.Constants.Globals)
 
 -- Create the constraint's instance and apply properties
-local function draw(hyp: number, origin: Vector2, thickness: number, parent: Instance, color: Color3, l: Frame?) : Frame
-	local line = l or Instance.new("Frame")
+local function draw(hyp: number, origin: Vector2, thickness: number, parent: Instance, color: Color3, l: Frame?, image: string?) : Frame
+	local line = l or (image and Instance.new("ImageLabel") or Instance.new("Frame"))
 	line.Name = "Constraint"
 	line.AnchorPoint = Vector2.new(.5, .5)
-	line.Size = UDim2.new(0, hyp, 0, thickness or Globals.constraint.thickness)
-	line.BackgroundColor3 = color or Globals.constraint.color
+	line.Size = UDim2.new(0, hyp, 0, (thickness or Globals.constraint.thickness) + (image and 15 or 0))
+	line.BackgroundTransparency = image and 1 or 0
 	line.BorderSizePixel = 0
 	line.Position = UDim2.fromOffset(origin.X, origin.Y)
 	line.ZIndex = 1
 	line.Parent = parent
-
+	
+	if image then 
+		line.Image = image
+		line.ImageColor3 = color or Globals.constraint.color
+	else 
+		line.BackgroundColor3 = color or Globals.constraint.color
+	end
+	
 	return line
 end
 
-return function (origin: Vector2, endpoint: Vector2, parent: Instance, thickness: number, color: Color3, l: Frame?) : Frame
+return function (origin: Vector2, endpoint: Vector2, parent: Instance, thickness: number, color: Color3, l: Frame?, image: string?) : Frame
 	-- Calculate magnitude between the constraint's points
 	-- Draw the constraint
 	-- Calculate rotation
 	local hyp = (endpoint - origin).Magnitude
-	local line = draw(hyp, origin, thickness, parent, color, l)
+	local line = draw(hyp, origin, thickness, parent, color, l, image)
 	local mid = (origin + endpoint)/2
 	local theta = math.atan2((origin - endpoint).Y, (origin - endpoint).X)	
 	
