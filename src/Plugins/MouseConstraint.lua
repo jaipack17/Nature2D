49a53367d89d7fdc1225d79510aabfba394de075
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2022-03-25 12:34:53 +0530

    Fix mouse constraint bug

--- src/Plugins/MouseConstraint.lua
+++ src/Plugins/MouseConstraint.lua
@@ -1,35 +1,21 @@
 local UserInputService = game:GetService("UserInputService")
 
 return function (engine: { any }, range: number)
-	local mousePoint = engine:Create("Point", {
-		Position = UserInputService:GetMouseLocation(),
-		Visible = false,
-		Snap = true,
-		KeepInCanvas = false
-	})
 	local held = nil
-	local constraint = nil
 
 	UserInputService.InputBegan:Connect(function(input, processedEvent)
 		if processedEvent then return end
 
-		if input.UserInputType == Enum.UserInputType.MouseButton1 and not held and not constraint then
+		if input.UserInputType == Enum.UserInputType.MouseButton1 and not held then
 			for _, b in ipairs(engine.bodies) do
 				for _, p in ipairs(b.vertices) do
 					if (p.pos - UserInputService:GetMouseLocation()).Magnitude <= range then
 						p.selectable = true
 						held = p
-						constraint = engine:Create("Constraint", {
-							Type = "Rod",
-							Point1 = held,
-							Point2 = mousePoint,
-							RestLength = 1,
-							Visible = false
-						})
 						break
 					end
 				end
-				
+
 				if held then
 					break
 				end
@@ -40,20 +26,18 @@ return function (engine: { any }, range: number)
 	UserInputService.InputEnded:Connect(function(input, processedEvent)
 		if processedEvent then return end
 
-		if input.UserInputType == Enum.UserInputType.MouseButton1 and held and constraint then
+		if input.UserInputType == Enum.UserInputType.MouseButton1 and held then
 			held.selectable = false
-			constraint:Destroy()
 			held = nil
-			constraint = nil
 		end
 	end)
 
 	UserInputService.InputChanged:Connect(function(input, processedEvent)
 		if processedEvent then return end
 
-		if input.UserInputType == Enum.UserInputType.MouseMovement then
+		if input.UserInputType == Enum.UserInputType.MouseMovement and held then
 			local mouse = UserInputService:GetMouseLocation()
-			mousePoint:SetPosition(mouse.X, mouse.Y)
+			held:SetPosition(mouse.X, mouse.Y)
 		end
 	end)
 end
